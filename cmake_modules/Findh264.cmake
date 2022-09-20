3df49c4f62e62514e83f1f61632465b125474ebb
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2022-09-20 16:35:02 +0200

    h264bitstream.cmake: Try making it cross compile
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- cmake_modules/Findh264.cmake
+++ cmake_modules/Findh264.cmake
@@ -1,16 +1,30 @@
-set (H264_INCLUDE_DIR /usr/local/include/h264bitstream)
-set (H264_LIB_DIR /usr/local/lib)
+set (H264_DIR ~/h264)
+#set (H264_INCLUDE_DIR /usr/local/include/h264bitstream)
+#set (H264_LIB_DIR /usr/local/lib)
 
-set(H264_FOUND TRUE)
-  
+
+#find_path(H264_INCLUDE_DIR
+#
+#)
+
+find_path(H264_LIB_DIR
+    libh264bitstream.so
+    PATHS ${H264_DIR}
+    PATH_SUFFIXES lib
+)
+
+if (H264_LIB_DIR)
+    set(H264_FOUND TRUE)
+endif()
+
 if (H264_FOUND)
     if (NOT h264_FIND_QUIETLY)
         message(STATUS "Found h264bitstream:")
-        message(STATUS " - Includes: ${H264_INCLUDE_DIR}")
+        #message(STATUS " - Includes: ${H264_INCLUDE_DIR}")
         message(STATUS " - Libraries: ${H264_LIB_DIR}")
     endif()
     add_library(h264 INTERFACE)
-    target_include_directories(h264 INTERFACE ${H264_INCLUDE_DIR})
+    #target_include_directories(h264 INTERFACE ${H264_INCLUDE_DIR})
     set_target_properties(h264 PROPERTIES INTERFACE_LINK_DIRECTORIES ${H264_LIB_DIR})
     target_link_libraries(h264 INTERFACE libh264bitstream.so)
 else()
